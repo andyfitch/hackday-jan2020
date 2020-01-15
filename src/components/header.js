76b88f4a379bb2343ab9c60590cdd231fd735e4d
Andy Fitch <andy@parall.ax>
2020-01-15 14:57:03 +0000

    Initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -8,14 +8,9 @@ function Header() {
     <header className="bg-teal-500">
       <div className="flex flex-wrap md:flex-no-wrap items-center justify-between max-w-4xl mx-auto p-4 md:p-8">
         <div className="flex items-center">
-          <img
-            src="tailwind-logo.svg"
-            className="mr-3 text-white w-10"
-          />
-
           <Link href="/">
             <a className="font-bold text-white text-xl">
-              Next.js Starter Tailwind
+              Hack Day Jan 2020
             </a>
           </Link>
         </div>
